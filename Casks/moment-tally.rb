94de4eb788a3f37cd574719968e918d0ed6c6f84
Steven Fitzpatrick <84053270+sf1tzp@users.noreply.github.com>
2026-09-02 20:50:15 -0700

    moment-tally 1.2.1 (#12)

--- Casks/moment-tally.rb
+++ Casks/moment-tally.rb
@@ -1,6 +1,6 @@
 cask "moment-tally" do
-  version "1.2.0"
-  sha256 "f07c6e4e5c91f75594e163fba8e03ac2b52de18256366228b38c49415a40c450"
+  version "1.2.1"
+  sha256 "52522283eee17fbc2f43f505409abfef45ac7ac2987fbf6787ad54e5ac3047b7"
 
   url "https://github.com/sf1tzp/moment-tally/releases/download/v#{version}/MomentTally-#{version}.zip"
   name "Moment Tally"
